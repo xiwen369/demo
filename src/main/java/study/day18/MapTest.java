3957e63cbfa852daf472540ee013313919bd041f
xiwen369 <38783437+xiwen369@users.noreply.github.com>
2019-05-16 17:14:45 +0800

    Map案例(计算字符串中的每一个字符出现的次数)

--- src/main/java/study/day18/MapTest.java
+++ src/main/java/study/day18/MapTest.java
@@ -1,11 +1,13 @@
 package study.day18;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 //Map案例(计算字符串中的每一个字符出现的次数)
 public class MapTest {
     public static void main(String[] args) {
-        HashMap<Character, Integer> map = new HashMap<>();
+//      Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new TreeMap<>();
         String str = "ashfjhquoewurhwehfjksdbfsjabakhfskfdhlsfh";
         char[] arr = str.toCharArray();
         System.out.println(arr);
